4eac3e60c44dd089b14c9b287c339476b53c07a8
yinyangshi <1418780411@qq.com>
2018-04-28 23:36:52 +0800

    honor!

--- src/com/github/euonmyoji/newhonor/PAPIManager.java
+++ src/com/github/euonmyoji/newhonor/PAPIManager.java
@@ -1,10 +1,27 @@
 package com.github.euonmyoji.newhonor;
 
 import me.rojo8399.placeholderapi.Placeholder;
+import me.rojo8399.placeholderapi.PlaceholderService;
 import me.rojo8399.placeholderapi.Source;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 
 public class PAPIManager {
+
+    PAPIManager() {
+        PlaceholderService service = Sponge.getServiceManager().provideUnchecked(PlaceholderService.class);
+        service.loadAll(this, this).stream().peek(builder -> {
+            if ("newhonor".equals(builder.getId())) {
+                builder.description("newhonor text").version("1.5").author("yinyangshi");
+            }
+        }).forEach(builder -> {
+            try {
+                builder.buildAndRegister();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
     @Placeholder(id = "newhonor")
     public Object getNewHonorText(@Source Player p) {
