2a626d38944d7e1d9a614f82123cf3c675e7cfe0
yinyangshi <1418780411@qq.com>
2018-05-13 11:08:46 +0800

    修复了版本号不是正式版的BUG

--- src/main/java/com/github/euonmyoji/newhonor/NewHonor.java
+++ src/main/java/com/github/euonmyoji/newhonor/NewHonor.java
@@ -45,7 +45,7 @@ import java.util.UUID;
 @Plugin(id = "newhonor", name = "New Honor", version = NewHonor.VERSION, authors = "yinyangshi", description = "NewHonor plugin",
         dependencies = {@Dependency(id = "ultimatechat", optional = true), @Dependency(id = "placeholderapi", optional = true)})
 public class NewHonor {
-    public static final String VERSION = "1.7.0-alpha";
+    public static final String VERSION = "1.7.0";
     public static final NewHonorMessageChannel M_MESSAGE = new NewHonorMessageChannel();
     @Inject
     @ConfigDir(sharedRoot = false)
