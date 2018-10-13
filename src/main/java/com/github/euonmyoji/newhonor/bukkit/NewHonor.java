2feaf7b928bba4391a69d084e0b240d357ac1ad2
yinyangshi <1418780411@qq.com>
2018-10-13 23:36:07 +0800

    开头一个类 剩下全靠催

--- src/main/java/com/github/euonmyoji/newhonor/bukkit/NewHonor.java
+++ src/main/java/com/github/euonmyoji/newhonor/bukkit/NewHonor.java
@@ -1,7 +1,15 @@
 package com.github.euonmyoji.newhonor.bukkit;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * @author NewHonor authors
  */
-public class NewHonor {
+public class NewHonor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //什么 还在用这个logger 告辞 我不写了(雾) #run
+        getLogger().info("NewHonor author email:1418780411@qq.com");
+    }
 }
