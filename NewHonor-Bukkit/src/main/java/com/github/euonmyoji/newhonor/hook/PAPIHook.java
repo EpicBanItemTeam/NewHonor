723dccd1c00cd2b4ba6e705ed94a01693822b5cf
MungSoup <MungSoup@yeah.net>
2018-11-04 14:51:35 +0800

    some bugs

--- NewHonor-Bukkit/src/main/java/com/github/euonmyoji/newhonor/hook/PAPIHook.java
+++ NewHonor-Bukkit/src/main/java/com/github/euonmyoji/newhonor/hook/PAPIHook.java
@@ -2,22 +2,30 @@ package com.github.euonmyoji.newhonor.hook;
 
 import com.github.euonmyoji.newhonor.NewHonor;
 import com.github.euonmyoji.newhonor.api.configuration.PlayerConfig;
+import com.github.euonmyoji.newhonor.data.Honor;
 import me.clip.placeholderapi.PlaceholderHook;
 import org.bukkit.entity.Player;
+
 //fixme: 包呢 库呢 没库写什么🐎
 public class PAPIHook extends PlaceholderHook {
 
     @Override
     public String onPlaceholderRequest(Player p, String params) {
-        if ("text".equals(params)) {
-            try {
-                PlayerConfig config = PlayerConfig.get(p.getUniqueId());
-                if (config.isUseHonor() && config.getUsingHonorID() != null) {
-                    return NewHonor.honorConfig.getHonor(config.getUsingHonorID()).getText();
+        try {
+            PlayerConfig config = PlayerConfig.get(p.getUniqueId());
+            if (config.isUseHonor() && config.getUsingHonorID() != null) {
+                Honor honor = NewHonor.honorConfig.getHonor(config.getUsingHonorID());
+                switch (params) {
+                    case "text":
+                        return honor.getText();
+                    case "click_value":
+                        return honor.getClickValue();
+                    case "hover_value":
+                        return honor.getHoverValue();
                 }
-            } catch (Exception e) {
-                return "";
             }
+        } catch (Exception e) {
+            return "";
         }
         return "";
     }
